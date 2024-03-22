d6e150e807fde37f0f2d3c38cfacbd42d6997df0
hasib72 <hasibrahaman72@gmail.com>
2024-03-22 11:55:11 +0530

    Mir's Branch

---
+++ server/model/SampleModel.js
@@ -0,0 +1,41 @@
+ const mongoose = require('mongoose')
+
+ const UserSchema = new mongoose.Schema({
+   first_name: {
+      type: String,
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid']
+
+  },
+  last_name: {
+      type: String,
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid']
+  },
+  user_name: {
+      type: String,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+      index: true,
+  },
+  password: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  email: {
+      type: String,
+
+  },
+  dob: {
+      type: Date,
+      
+  },
+  bio: {
+      type: String,
+      
+  }
+ })
+
+ const UserModel = mongoose.model("User", UserSchema)
+ 
+ module.exports = UserModel
