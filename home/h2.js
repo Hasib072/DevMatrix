7fcb90ce14df6d455ea878abcd9b44134f815879
MonsieurJoshua <joshua1eapen2004@gmail.com>
2024-03-26 11:12:34 +0530

    carousel

---
+++ home/h2.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slides img');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+}
+
+
+setInterval(nextSlide, 2000);
+showSlides();
---
+++ home/home.js
@@ -0,0 +1,15 @@
+function showSlides() {
+    const slides = document.querySelectorAll('.slides img');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+}
+
+
+setInterval(nextSlide, 2000);
+showSlides();
